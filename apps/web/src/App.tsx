d7d3606ed8ea44a3586d797596de33d2835fed8a
Joao Vitor Cirelli <67876054+StormYasta@users.noreply.github.com>
2026-08-26 21:26:24 -0300

    feat(web): route programming and subjects modules separately

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -19,7 +19,11 @@ import { ChallengeSolve } from './pages/ChallengeSolve';
 import { Rankings } from './pages/Rankings';
 import { TeacherDashboard } from './pages/TeacherDashboard';
 import { TeacherChallenges } from './pages/TeacherChallenges';
+import { TeacherAcademicExercises } from './pages/TeacherAcademicExercises';
 import { GeneralChallenges } from './pages/GeneralChallenges';
+import { PracticeHub } from './pages/PracticeHub';
+import { AcademicExercises } from './pages/AcademicExercises';
+import { AcademicExerciseSolve } from './pages/AcademicExerciseSolve';
 import { Backoffice } from './pages/Backoffice';
 
 export const App: React.FC = () => {
@@ -33,7 +37,9 @@ export const App: React.FC = () => {
 
             <Route element={<Layout />}>
               <Route path="/" element={<Dashboard />} />
-              <Route path="/practice" element={<GeneralChallenges />} />
+              <Route path="/practice" element={<PracticeHub />} />
+              <Route path="/practice/programming" element={<GeneralChallenges />} />
+              <Route path="/practice/subjects" element={<AcademicExercises />} />
               <Route path="/learning-paths" element={<LearningPaths />} />
               <Route path="/learning-paths/:slug" element={<LearningPathDetail />} />
               <Route path="/assignments" element={<Assignments />} />
@@ -46,11 +52,13 @@ export const App: React.FC = () => {
               <Route path="/codewars-import/bulk" element={<CodewarsBulkImport />} />
               <Route path="/teacher/dashboard" element={<TeacherDashboard />} />
               <Route path="/teacher/challenges" element={<TeacherChallenges />} />
+              <Route path="/teacher/academic-exercises" element={<TeacherAcademicExercises />} />
               <Route path="/backoffice" element={<Backoffice />} />
               <Route path="/challenges/:idOrSlug" element={<ChallengeSolve />} />
+              <Route path="/academic-exercises/:idOrSlug" element={<AcademicExerciseSolve />} />
 
-              <Route path="/general" element={<Navigate to="/practice" replace />} />
-              <Route path="/general/challenges" element={<Navigate to="/practice" replace />} />
+              <Route path="/general" element={<Navigate to="/practice/programming" replace />} />
+              <Route path="/general/challenges" element={<Navigate to="/practice/programming" replace />} />
               <Route path="/academic/dashboard" element={<Navigate to="/" replace />} />
             </Route>
 
